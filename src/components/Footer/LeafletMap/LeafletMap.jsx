0545d95d69f8a33751660406bacf86459ea93f43
Tanzeebul-Tamim <tamim200091@gmail.com>
2023-05-20 10:41:42 +0600

    footer done

---
+++ src/components/Footer/LeafletMap/LeafletMap.jsx
@@ -0,0 +1,29 @@
+import "./LeafletMap.css";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet"; 
+import osm from "./osm-providers";
+import "leaflet/dist/leaflet.css";
+import { useRef } from "react";
+
+
+const LeafletMap = () => {
+  const center ={ lat: 23.795, lng: 90.40540631122356};
+  const ZOOM_LEVEL = 14.5;
+  const mapRef = useRef();
+
+  return (
+        <MapContainer scrollWheelZoom={false} center={center} zoom={ZOOM_LEVEL} ref={mapRef}>
+            <TileLayer url={osm.maptiler.url} attribution={osm.maptiler.attribution} />
+            <Marker position={[23.79382753409999, 90.40540631122356]}>
+                <Popup>
+                  FluffyFriends
+                  <br />
+                  Level-4, 34, Awal Centre,
+                  <br />
+                  Banani, Dhaka
+                </Popup>
+            </Marker>
+        </MapContainer>
+  );
+};
+
+export default LeafletMap;
